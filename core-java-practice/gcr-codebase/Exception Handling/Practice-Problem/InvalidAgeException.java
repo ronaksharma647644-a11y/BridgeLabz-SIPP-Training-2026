ee99fcef57f89ead415c5e62dc21ae3eefa44c20
ronaksharma647644-a11y <ronaksharma647644@gmail.com>
2026-06-19 19:17:28 +0530

    [Ronak Sharma] Added Exception Handling Assignment

---
+++ core-java-practice/gcr-codebase/Exception Handling/Practice-Problem/InvalidAgeException.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
---
+++ core-java-practice/gcr-codebase/Exception Handling/Scenario-Based/OnlineQuizApplication.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class OnlineQuizApplication {
+
+    public static void main(String[] args) {
+
+        String[] answers = {"A", "B", "C", "D"};
+
+        for (int i = 0; i < 6; i++) {
+
+            try {
+
+                if (answers[i].equals("A")) {
+                    System.out.println("Correct");
+                }
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Invalid answer index");
+            } catch (NullPointerException e) {
+                System.out.println("Answer is null");
+            }
+        }
+    }
+}
